07b7d3cfc718cabfa918231ab447f9d39d9c6ceb
Your name <adhuard@mail.ru>
2024-08-23 14:54:43 +0300

    js

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".slider-wrapper").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        Infinity: true,
+        centerMode: true,
+        centerPadding: "102px", /* отступ от 3х уже видимых слайдов*/
+    })
+    $(".slick-prev").text(">");
+    $(".slick-next").text(">");
+
+
+})
